ebffd1876dd8152f9464314c0aa7f1020e354b09
VienaCampo15 <vienacampo15@gmail.com>
2022-03-15 22:58:23 -0500

    Viena Campo, David Mendoza

---
+++ my-products-api/my_products_api/models/schemas.py
@@ -0,0 +1,17 @@
+from pydantic import BaseModel
+
+class Product(BaseModel):
+    id: int
+    name: str
+    quantity: int
+    description: str
+    price: int
+    category: int 
+
+
+class ProductCreate(BaseModel):
+    name: str
+    quantity: int
+    description: str
+    price: int
+    category: int
